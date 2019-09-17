2f22af800afc5c471f4a16a501d6d53a15a354ed
Shivaot <shivatiwari757@gmail.com>
2019-09-17 14:28:36 +0530

    adding factory beans

---
+++ src/main/java/guru/springframework/didemo/services/GreetingRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.didemo.services;
+
+public interface GreetingRepository {
+
+    String getEnglishGreeting();
+    String getGermanGreeting();
+    String getSpanishGreeting();
+}
